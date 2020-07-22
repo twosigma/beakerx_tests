5772b79bb71fee833e13cfe338469e21baa073f6
EfimovVladimir <VEffimov@qulix.com>
2020-07-22 11:46:47 -0700

    revert changes

--- autotests/src/test/java/com/twosigma/beakerx/autotests/NotebookPO.java
+++ autotests/src/test/java/com/twosigma/beakerx/autotests/NotebookPO.java
@@ -17,7 +17,6 @@
 package com.twosigma.beakerx.autotests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
